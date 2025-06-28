2c6afbc3fdb959a79375beb771a915b71eabae08
TriTruong14 <truongminh13@gmail.com>
2025-06-28 16:15:03 +0700

    Cập nhật bài tập 1 (cấp độ 2)

--- src/test/java/oop/CapDo2_BaiTap1/ThueTNCA.java
+++ src/test/java/oop/CapDo2_BaiTap1/ThueTNCA.java
@@ -7,35 +7,15 @@ import java.util.Scanner;
 public class ThueTNCA {
     public static void main(String[] args) {
 
-        //input - nhập thu nhập hằng năm
+        //input - Nhập thu nhập hằng năm
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập thu nhập hằng năm của bạn: ");
         double TN = scanner.nextInt();
 
         //process - Tính số thuế phải trả theo từng mức thuế suất.
-//        static String thuNhap ( double TN){
-//            int result;
-//            if (TN >= 0 && TN <= 5) {
-//                result = (5);
-//            } else if (TN <= 6 && TN >= 10) {
-//                result = (10);
-//            } else if (TN <= 11 && TN >= 18) {
-//                result = (15);
-//            } else if (TN <= 19 && TN >= 32) {
-//                result = (20);
-//            } else if (TN <= 33 && TN >= 52) {
-//                result = (25);
-//            } else if (TN <= 53 && TN >= 80) {
-//                result = (30);
-//            } else if (TN >= 81) {
-//                result = (35);
-//            }
-//            return result;
-
         double tienThue = 1234567.89;
         NumberFormat currencyFormatUS = NumberFormat.getCurrencyInstance(Locale.US);
         NumberFormat currencyFormatVN = NumberFormat.getCurrencyInstance(new Locale("vi", "VN")); // Locale cho Việt Nam
-//        double tienThue = 0;
             if (TN >= 0 && TN <= 5000000) {
                 tienThue = TN * 5 / 100;
             } else if (TN >= 6000000 && TN <= 10000000) {
