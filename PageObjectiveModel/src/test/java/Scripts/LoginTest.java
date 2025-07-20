b553b41f253d1a46e7d3afbc2bf879725228ee24
TriTruong14 <truongminh13@gmail.com>
2025-07-20 11:44:05 +0700

    update

--- PageObjectiveModel/src/test/java/Scripts/LoginTest.java
+++ PageObjectiveModel/src/test/java/Scripts/LoginTest.java
@@ -8,7 +8,8 @@ import utils.ExcelReader;
 public class LoginTest extends BaseTest {
     @DataProvider(name = "loginData") //cung cấp dữ liệu đầu vào cho các @test
     public Object[][] loginData() {
-        String filePath = "PageObjectiveModel/src/test/java/Resources/loginData.xlsx";
+        String filePath = "src/test/java/Resources/loginData.xlsx"; //PageObjectiveModel/src/test/java/Resources
+
         String sheetName = "Sheet1";
 
         //tạo biến số dòng dữ liệu, không bao gồm tiêu đề
@@ -33,7 +34,7 @@ public class LoginTest extends BaseTest {
             LoginPage loginPage = new LoginPage(driver);
             loginPage.login(username, password);
 
-            Thread.sleep(2000);
+            Thread.sleep(3000);
             Boolean isLogged = driver.getCurrentUrl().contains("dashboard");
             System.out.println(isLogged);
             if (Boolean.parseBoolean(expectedResult) == isLogged) {
@@ -46,3 +47,49 @@ public class LoginTest extends BaseTest {
         }
     }
 }
+
+//public class LoginTest extends BaseTest {
+//    @DataProvider(name = "loginData") // cung cấp dữ liệu đầu vào cho các @test
+//    public Object[][] loginData() {
+//        String filePath = "src/test/java/resources/loginData.xlsx";
+//        String sheetName = "Sheet1";
+//
+//        //tạo biến số dòng dữ liệu (không bao gồm dòng tiêu đề)
+//        int rowCount = 4;
+//
+//        //tạo mảng 2 chiều để lưu dữ liệu
+//        Object[][] data = new Object[rowCount][3];
+//
+//        for (int i = 0; i < rowCount; i++) {
+//            //lấy dữ liệu cho username
+//            data[i][0] = ExcelReader.getCellData(filePath, sheetName, i + 1, 0);
+//
+//            //lấy dữ liệu cho password
+//            data[i][1] = ExcelReader.getCellData(filePath, sheetName, i + 1, 1);
+//
+//            //expectedResult
+//            data[i][2] = ExcelReader.getCellData(filePath, sheetName, i + 1, 2);
+//        }
+//        //trả dữ liệu ra ngoài
+//        return data;
+//    }
+//
+//    @Test(dataProvider = "loginData")
+//    public void loginTest(String username, String password, String expectedResult) {
+//        try {
+//            LoginPage loginPage = new LoginPage(driver);
+//            loginPage.login(username, password);
+//
+//            Thread.sleep(2000);
+//            Boolean isLogged = driver.getCurrentUrl().contains("dashboard");
+//            System.out.println(isLogged);
+//            if (Boolean.parseBoolean(expectedResult) == isLogged) {
+//                System.out.println("test pass");
+//            } else {
+//                System.out.println("test fail");
+//            }
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
--- PageObjectiveModel/src/test/java/utils/ExcelReader.java
+++ PageObjectiveModel/src/test/java/utils/ExcelReader.java
@@ -1,10 +1,7 @@
 package utils;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
